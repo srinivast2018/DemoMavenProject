b0b0dd76c116c702a97d54c57d0ac9a0f5d8ea36
srinivast2018 <srinivas.nirmal@gmail.com>
2019-01-10 13:08:30 +0530

    firstcommit

---
+++ src/test/java/com/ibm/basetest/BaseTest.java
@@ -0,0 +1,13 @@
+package com.ibm.basetest;
+
+import org.testng.annotations.Test;
+
+public class BaseTest {
+
+	@Test
+	public void LoginTest() 
+	{
+		System.out.println("Demo of Maven project");
+	}
+	
+}
